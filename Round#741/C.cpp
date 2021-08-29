01fc482c5a51f29259effa140510f02c92066a53
anzanda <tjd1024@g.skku.edu>
2021-08-29 07:45:06 +0000

    C first submit

---
+++ Round#741/C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define FastIO cin.tie(0)->sync_with_stdio(0)
+
+int len;
+string s;
+void input() {
+	cin >> len;
+	cin >> s;
+}
+void solve() {
+	bool flag = false;
+	for(int i=0; i<len; i++) {
+		if(s[i] != '1') {
+			flag = true;
+			break;
+		}
+	}
+	if(!flag) { // all of char is '1'
+		cout << 1 << " " << len/2 << endl;
+		cout << len - len/2 + 1<< " " << len << endl;
+		return;
+	}
+	for(int i=0; i<len/2; i++) {
+		if(s[i] == '0') {
+			cout << i+1 << " " << len << endl;
+			cout << i+1+1 << " " << len << endl;
+			return;
+		}
+	}
+	for(int i=len/2; i<len; i++) {
+		if(s[i] == '0') {
+			cout << 1 << " " << i+1 << endl;
+			cout << 1 << " " << i+1+1 << endl; 
+		}
+	}
+}
+int main(void) {
+	FastIO;
+	int T;
+	cin >> T;
+	while(T--) {
+		input();
+		solve();
+	}
+	return 0;
+}
