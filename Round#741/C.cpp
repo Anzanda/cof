d10ae205564291ff16bf1f6214cff092f9cbc093
anzanda <tjd1024@g.skku.edu>
2021-08-29 10:36:52 +0000

    C forth submit

--- Round#741/C.cpp
+++ Round#741/C.cpp
@@ -19,21 +19,21 @@ void solve() {
 		}
 	}
 	if(!flag) { // all of char is '1'
-		cout << 1 << " " << len/2 << " ";
-		cout << len - len/2 + 1<< " " << len << endl;
+		cout << 1 << " " << len-1;
+		cout << 2 << " " << len;
 		return;
 	}
 	for(int i=0; i<len/2; i++) {
 		if(s[i] == '0') {
-			cout << i+1 << " " << len << " ";
-			cout << i+1+1 << " " << len << endl;
+			cout << i+2 << " " << len << " ";
+			cout << i+1 << " " << len << endl;
 			return;
 		}
 	}
 	for(int i=len/2; i<len; i++) {
 		if(s[i] == '0') {
-			cout << 1 << " " << i+1+1 << " ";
-			cout << 1 << " " << i+1 << endl; 
+			cout << 1 << " " << i+1 << " ";
+			cout << 1 << " " << i << endl; 
 		}
 	}
 }
